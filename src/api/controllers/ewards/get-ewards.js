82b3c8b7503c25cf4622abbb9371b1e6058abe84
vinyasbj <vinyas555@gmail.com>
2023-12-22 13:00:01 +0530

    Fix: logger removed

--- src/api/controllers/ewards/get-ewards.js
+++ src/api/controllers/ewards/get-ewards.js
@@ -6,7 +6,6 @@ export default async (req, res) => {
     return res.status(500).json(errorHelper('00088', req, err.message));
   });
 
-  logger('00089', req.ewards, getText('en', '00089'), 'Info', req);
   return res.status(200).json({
     resultMessage: { en: getText('en', '00089'), tr: getText('tr', '00089') },
     resultCode: '00089',
