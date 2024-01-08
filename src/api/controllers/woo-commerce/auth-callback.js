785f1e1d206b405ce4e1b44b6c1809e38d84b90a
vinyasbj <vinyas555@gmail.com>
2024-01-08 21:49:13 +0530

    Fix: variable name changes to camel case and tested in local

--- src/api/controllers/woo-commerce/auth-callback.js
+++ src/api/controllers/woo-commerce/auth-callback.js
@@ -3,18 +3,18 @@ import { errorHelper, logger, getText } from '../../../utils/index.js';
 
 export default async (req, res) => {
   let body = req.body;
-  const store_url = req.body.user_id;
+  const storeUrl = req.body.user_id;
 
-  const woo_commerce = await WooCommerce.findOneAndUpdate({ store_url: store_url },body)
+  const wooCommerce = await WooCommerce.findOneAndUpdate({ store_url: storeUrl },body)
   .catch((err) => {
     return res.status(500).json(errorHelper('00041', req, err.message));
   });
 
-  if (!woo_commerce) return res.status(409).json(errorHelper('00004', req));
+  if (!wooCommerce) return res.status(409).json(errorHelper('00004', req));
 
-  logger('00006', woo_commerce._id, getText('en', '00006'), 'Info', req,"WooCommerce");
+  logger('00006', wooCommerce._id, getText('en', '00006'), 'Info', req,"WooCommerce");
   return res.status(200).json({
     resultMessage: { en: getText('en', '00006') },
-    resultCode: '00006', woo_commerce
+    resultCode: '00006', woo_commerce: wooCommerce
   });
 }
