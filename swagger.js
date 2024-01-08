3766aca22517f50174a3d125a7ba1e939cca40e5
Santosh Kumar <santosh.283143@gmail.com>
2024-01-08 15:41:11 +0530

    configured api doc for ewards-key apis

--- swagger.js
+++ swagger.js
@@ -1,14 +1,18 @@
 
-import  swaggerAutogen from "swagger-autogen"
+import swaggerAutogen from "swagger-autogen"
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    title: 'Kloc-Ewards',
+    description: 'Description',
   },
-  host: 'localhost:3001'
+  host: 'localhost:3001/api/ewards-key',
+  tags: [
+    { name: 'ewards-key', description: 'all ewards-key APIs' }
+  ],
+  paths: {}, // Will be populated by `swagger-autogen`
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['src/api/routes/index.js'];
+const routes = ['src/api/routes/ewards-key.js'];
 
 swaggerAutogen(outputFile, routes, doc);
