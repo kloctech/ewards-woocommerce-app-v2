559b0c43beb73623f2345a0dd714b3c5e039f78f
Santosh Kumar <santosh.283143@gmail.com>
2024-01-17 16:22:32 +0530

    changes after first pull request

--- src/api/controllers/ewards-key/create-ewards-key.js
+++ src/api/controllers/ewards-key/create-ewards-key.js
@@ -2,23 +2,24 @@ import { EwardsKey } from "../../../models/index.js";
 import { errorHelper, logger, getText } from "../../../utils/index.js";
 
 export default async (req, res) => {
-  const { merchantExists, wooCommerceExists } = req;
+  const { ewardsKey } = req;
 
-  let ewardsKey = new EwardsKey(req.body);
-  ewardsKey.ewards_merchant_id = merchantExists._id;
-  ewardsKey.woo_commerce_id = wooCommerceExists._id;
-
-  ewardsKey = await ewardsKey.save().catch((err) => {
-    logger("00096", '', getText("en", "00096"), "Error", req, "EwardsKey");
-    return res.status(500).json(err.errors);
-  });
-
-  logger("00021", ewardsKey._id, getText("en", "00021"), "Info", req, "EwardsKey");
-  return res.status(200).json({
-    resultMessage: { en: getText("en", "00021") },
-    resultCode: "00021",
-    ewardsKey,
-  });
+  await EwardsKey.create(ewardsKey)
+    .then(ewardsKeyResponse => {
+      logger("00021", ewardsKeyResponse._id, getText("en", "00021"), "Info", req, "EwardsKey");
+      return res.status(200).json({
+        resultMessage: { en: getText("en", "00021") },
+        resultCode: "00021",
+        ewardsKey,
+      });
+    })
+    .catch((err) => {
+      logger("00096", '', getText("en", "00096"), "Error", req, "EwardsKey");
+      return res.status(500).json({
+        resultMessage: { en: getText("en", "00096") },
+        resultCode: "00096",
+      });
+    });
 };
 
 
--- src/api/controllers/ewards-key/update-ewards-key.js
+++ src/api/controllers/ewards-key/update-ewards-key.js
@@ -3,19 +3,11 @@ import { errorHelper, logger, getText } from "../../../utils/index.js";
 
 export default async (req, res) => {
   const ewardsKeyId = req.params.id;
-  const { merchantExists, wooCommerceExists } = req;
-  const { customer_key, x_api_key, notes, merchant_id, store_url } = req.body
-  const ewardsKey = {
-    ...(customer_key && { customer_key }),
-    ...(x_api_key && { x_api_key }),
-    ...(notes && { notes }),
-    ...(merchant_id && { ewards_merchant_id: merchantExists }),
-    ...(store_url && { woo_commerce_id: wooCommerceExists }),
-  };
+  const { ewardsKey } = req;
 
-  const result = await EwardsKey.findByIdAndUpdate({ _id: ewardsKeyId }, ewardsKey)
+  await EwardsKey.findByIdAndUpdate({ _id: ewardsKeyId }, ewardsKey)
     .then(result => {
-      logger("00022", ewardsKey._id, getText("en", "00022"), "Info", req, "EwardsKey");
+      logger("00022", ewardsKeyId, getText("en", "00022"), "Info", req, "EwardsKey");
       return res.status(200).json({
         resultMessage: { en: getText("en", "00022") },
         resultCode: "00022",
@@ -30,10 +22,10 @@ export default async (req, res) => {
             resultCode: "00023",
           });
         } else {
-          logger("00097", ewardsKey._id, getText("en", "00097"), "Error", req, "EwardsKey");
+          logger("00097", ewardsKeyId, getText("en", "00097"), "Error", req, "EwardsKey");
           return res.status(500).json({
-            resultMessage: { en: getText("en", "00098") },
-            resultCode: "00098",
+            resultMessage: { en: getText("en", "00097") },
+            resultCode: "00097",
           });
         }
       }
