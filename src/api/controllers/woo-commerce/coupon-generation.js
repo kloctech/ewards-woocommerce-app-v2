75070ac40626ee03c1cadfda3935c5fda43b8bf5
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2024-02-09 12:33:40 +0530

    Added : Modified production keys and storeUrl in controller

--- src/api/controllers/woo-commerce/coupon-generation.js
+++ src/api/controllers/woo-commerce/coupon-generation.js
@@ -3,9 +3,9 @@ import { CreateCouponService } from "../../services/woo-commerce/index.js";
 export default async (req, res) => {
   const body = req.body;
 
-  const consumerKey = "ck_586c5dc474197573700f74cba2ebedfe3fce009f";
-  const consumerSecret = "cs_2473b1352f4112e873b1c2650ff28db92fce868a";
-  const storeUrl = "http://kloc-commerce.local";
+  const consumerKey = "ck_947602f0c79beb9da3b6e827767687451bec6a8a";
+  const consumerSecret = "cs_e999f4af83cfe1e8f607e007a2f44b1afa718115";
+  const storeUrl = "https://wordpress-698237-3804107.cloudwaysapps.com";
 
   try {
     const member = new CreateCouponService(consumerKey, consumerSecret, storeUrl, body.minimum_amount, body.email_restrictions, body.usage_limit, body.usage_limit_per_user, body.bill_amount, body.coupon_details, body.points, body.mobile_number, body.cart_id);
