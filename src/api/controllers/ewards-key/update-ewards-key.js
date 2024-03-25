711c56f5a1538cfef192ada040df39e8945f171c
Santosh Kumar <santosh.283143@gmail.com>
2024-03-25 15:42:40 +0530

    Feat : Added merchant id update functionality

--- src/api/controllers/ewards-key/update-ewards-key.js
+++ src/api/controllers/ewards-key/update-ewards-key.js
@@ -4,7 +4,7 @@ import { errorHelper, logger, getText } from "../../../utils/index.js";
 export default async (req, res) => {
   const { id: ewardsKeyId } = req.params;
   const { merchant, wooCommerce } = req;
-  const { customer_key, x_api_key, notes } = req.body
+  const { customer_key, x_api_key, notes, newMerchantId } = req.body
 
   let ewardsKey = await EwardsKey.findOne({ _id: ewardsKeyId }).catch(
     (err) => {
@@ -21,22 +21,29 @@ export default async (req, res) => {
     }
   );
 
-  try {
-    ewardsKey.ewards_merchant_id = merchant._id;
-    ewardsKey.woo_commerce_id = wooCommerce._id;
-    ewardsKey.customer_key = customer_key;
-    ewardsKey.x_api_key = x_api_key;
-    if (notes) ewardsKey.notes = notes;
+  ewardsKey.ewards_merchant_id = merchant._id;
+  ewardsKey.woo_commerce_id = wooCommerce._id;
+  ewardsKey.customer_key = customer_key;
+  ewardsKey.x_api_key = x_api_key;
+  if (notes) ewardsKey.notes = notes;
 
-    await ewardsKey.save();
-    logger('00022', ewardsKey._id, getText('en', '00022'), 'Info', req, "EwardsKey");
-    return res.status(200).json({
-      resultMessage: { en: getText('en', '00022') },
-      resultCode: '00022',
-      ewards_key: ewardsKey
-    });
-  } catch (err) {
+  await ewardsKey.save().catch(err => {
     logger('00097', '', getText('en', '00097'), 'Error', req, "EwardsKey");
     return res.status(500).json(errorHelper('00097', req, err.message));
-  }
+  })
+  logger('00022', ewardsKey._id, getText('en', '00022'), 'Info', req, "EwardsKey");
+
+  merchant.merchant_id = newMerchantId;
+  await merchant.save().catch(err => {
+    logger('00131', '', getText('en', '00131'), 'Error', req, "EwardsMerchant");
+    return res.status(500).json(errorHelper('00131', req, err.message));
+  })
+  logger('00132', merchant._id, getText('en', '00132'), 'Info', req, "EwardsMerchant");
+
+  return res.status(200).json({
+    resultMessage: { en: getText('en', '00022') },
+    resultCode: '00022',
+    ewards_key: ewardsKey,
+    merchant
+  });
 };
