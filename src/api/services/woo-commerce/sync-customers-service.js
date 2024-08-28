df9046c3a2e496f895684d691b394cddc74fc6f8
Santosh Kumar <santosh.283143@gmail.com>
2024-08-28 14:45:18 +0530

    Update : Syncing subscriber data along with customer data after plugin installation

--- src/api/services/woo-commerce/sync-customers-service.js
+++ src/api/services/woo-commerce/sync-customers-service.js
@@ -21,13 +21,20 @@ export default class SyncCustomersService {
   }
 
   async execute() {
-    let response = await this.WooCommerce.get("customers?page=1&per_page=100").catch((error) => {
-      console.log(error.response);
+    let customerResponse = await this.WooCommerce.get("customers", { per_page: 100, page: 1, role: "customer" }).catch((error) => {
+      console.log(error.message);
     });
-    await this.#createCustomers(response.data);
+    let subscriberResponse = await this.WooCommerce.get("customers", { per_page: 100, page: 1, role: "subscriber" }).catch((error) => {
+      console.log(error.message);
+    });
+
+    await this.#createCustomers([...customerResponse.data, ...subscriberResponse.data]);
     logger("00027", this.wooCommerceId, getText("en", "00027"), "Info", "", "Customer");
-    let nextUrl = await this.#getNextPageUrl(response.headers.link);
-    await this.#fetchCustomers(nextUrl);
+
+    let nextUrlCustomers = await this.#getNextPageUrl(customerResponse.headers.link, "customer");
+    let nextUrlSubscribers = await this.#getNextPageUrl(subscriberResponse.headers.link, "subscriber");
+    await this.#fetchCustomers(nextUrlCustomers);
+    await this.#fetchCustomers(nextUrlSubscribers);
   }
 
   async #fetchCustomers(nextPageUrl) {
